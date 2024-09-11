39d24074ba2c1295ce0e2e051859c857570928f8
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2024-09-10 22:06:01 -0600

    Practica terminada

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 
 Implementaciones de la práctica cuatro de Criptografía y Seguridad
 
+Para descrifrar los archivos ejecutar el siguiente comando:
+
+```bash
+python3 main.py fileN.lol
+```
+
+Con N del 2 al 4
+
+Para descifrar el archivo 1 ejecutar el siguiente comando:
+
+```bash
+python3 main.py file1.txt
+```
--- src/decrypt_png.py
+++ src/decrypt_png.py
@@ -1,18 +1,37 @@
 base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
-
 def read_file(file_path):
+    """
+    Lee un archivo y devuelve su contenido en bytes.
+
+    Args:
+        file_path (str): La ruta del archivo a leer.
+
+    Returns:
+        bytes: El contenido del archivo.
+    """
     with open(file_path, 'rb') as file:
         return file.read()
 
 def base64_decode(encrypted_information):
+    """
+    Decodifica una cadena codificada en base64.
+
+    Args:
+        encrypted_information (str or bytes): La información codificada en base64.
+
+    Returns:
+        bytes: La información decodificada.
+    """
     if isinstance(encrypted_information, bytes):
         encrypted_information = encrypted_information.decode('utf-8')
     encrypted_information = encrypted_information.rstrip('=')
     bit_string = ''.join(f'{base64_chars.index(c):06b}' for c in encrypted_information if c in base64_chars)
     return bytes(int(bit_string[i:i+8], 2) for i in range(0, len(bit_string), 8) if len(bit_string[i:i+8]) == 8)
 
+# Lee el archivo codificado en base64
 decrypted_data = base64_decode(read_file('file1.lol'))
 
+# Guarda los datos decodificados en un archivo PNG
 with open('file1.png', 'wb') as file:
     file.write(decrypted_data)
